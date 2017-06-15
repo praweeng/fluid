fe844a287585d877d18c48161621838baa7e8397
Praveen Gonthina <praveengonthina@gmail.com>
2017-06-15 15:43:27 +0530

    Add files via upload

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import { AppRegistry } from 'react-native';
+import Fluid from './src/app';
+
+AppRegistry.registerComponent('Fluid', () => Fluid);
